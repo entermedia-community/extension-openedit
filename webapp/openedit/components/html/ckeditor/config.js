78706b62658e0932ce1de27dfb62b858f6590ae2
Christopher Burkey <cburkey@openedit.org>
2014-01-16 14:47:41 -0500

    Addded permission check and background colors

--- webapp/openedit/components/html/ckeditor/config.js
+++ webapp/openedit/components/html/ckeditor/config.js
@@ -7,7 +7,32 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For the complete reference:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	
+	//config.toolbar = 'Full';
+	 
+	config.toolbar =
+	[
+	 	{ name: 'save', items : [ 'savebtn']},
+		{ name: 'document', items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+		{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+		{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+		{ name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 
+	        'HiddenField' ] },
+		{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+		{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+	 	{ name: 'cancel', items : [ 'closebtn' ]},
+		'/',
+		{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
 
+		{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+		{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+		'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+		{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+		{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+	];
+	 
+	
+	/*
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
 		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
@@ -22,13 +47,15 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
 		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
 		{ name: 'styles' },
-		{ name: 'colors' },
+		{ name: 'colors' , items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', groups: [ 'Maximize', 'ShowBlocks' ] },
 		{ name: 'about' }
 	];
+	*/
 
 	// Remove some buttons, provided by the standard plugins, which we don't
 	// need to have in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	//config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Se the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
@@ -39,7 +66,7 @@ CKEDITOR.editorConfig = function( config ) {
 	config.allowedContent = true; 
 	
 	//config.extraPlugins = 'ajaxsave';//savebtn is the plugin's name
-	config.extraPlugins='savebtn,closebtn';		 	
+	config.extraPlugins='savebtn,closebtn,colorbutton,panelbutton';		 	
 	
 	
 };
