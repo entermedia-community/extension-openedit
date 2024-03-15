0e6793776d0c32c1f753ee613ee6d978efa3da74
Ian <ian@oneliveweb.com>
2024-03-14 19:36:25 -0600

    Revert "asdf"
    This reverts commit 9194b3338886730e6a7ab281031bc16429fa3b44.

--- src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        MessageProvider todayProvider = new TodayMessageProvider();
-        MessageProvider tomorrowProvider = new TomorrowMessageProvider();
-        
-        System.out.println(todayProvider.getMessage());
-        System.out.println(tomorrowProvider.getMessage());
-    }
-}
--- src/MessageProvider.java
+++
@@ -1,3 +0,0 @@
-public interface MessageProvider {
-    String getMessage();
-}
